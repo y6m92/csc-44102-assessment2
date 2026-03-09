73270f53968f8939d89793097063aa877be261e2
y6m92 <y6m92@students.keele.ac.uk>
2026-03-09 01:52:41 +0000

    Add GUI labels and input box

--- game_gui.py
+++ game_gui.py
@@ -1,6 +1,33 @@
 import tkinter as tk
+import random
 
-window = tk.Tk()
-window.title("Number Guessing Game")
+#Create game window
+app = tk.Tk()
+app.title("Number Guessing Game")
 
-window.mainloop()
+#Game values
+number = random.randint(1,20)
+tries = 0
+max_tries = 5
+
+#title
+title = tk.Label(app, text="Guess the number between 1 and 20")
+title.pack()
+
+#attempts   
+tries_label = tk.Label(app, text=f"Attempts: {tries}/{max_tries}")
+tries_label.pack()
+
+#input box
+entry = tk.Entry(app)
+entry.pack()
+
+#feedback text 
+feedback_label = tk.Label(app, text="")
+feedback_label.pack()
+
+#button 
+submit = tk.Button(app, text="Submit guess")
+submit.pack()
+
+app.mainloop()
