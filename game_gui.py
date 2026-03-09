b3b64daa2db38a03177447812f66b963dd45a6fc
y6m92 <y6m92@students.keele.ac.uk>
2026-03-09 02:36:08 +0000

    Add basic game logic to GUI

--- game_gui.py
+++ game_gui.py
@@ -10,6 +10,31 @@ number = random.randint(1,20)
 tries = 0
 max_tries = 5
 
+def check_guess():
+    global tries
+    guess = entry.get()
+    
+    if not guess.isdigit():
+        feedback_label.config(text="Enter a number.")
+        return
+    
+    guess = int(guess)
+    tries += 1
+    tries_label.config(text=f"Attempts: {tries}/{max_tries}")
+    
+    if guess < number:
+        feedback_label.config(text="Too low.")
+    elif guess > number:
+        feedback_label.config(text="Too high.")
+    else:
+        feedback_label.config(text="Correct!")
+        submit.config(state="disabled")
+        return
+    
+    if tries == max_tries:
+        feedback_label.config(text=f"Out of tries. Number was {number}.")
+        submit.config(state="disabled")
+        
 #title
 title = tk.Label(app, text="Guess the number between 1 and 20")
 title.pack()
@@ -27,7 +52,7 @@ feedback_label = tk.Label(app, text="")
 feedback_label.pack()
 
 #button 
-submit = tk.Button(app, text="Submit guess")
+submit = tk.Button(app, text="Submit guess", command=check_guess)
 submit.pack()
 
 app.mainloop()
