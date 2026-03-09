03f75282ccac5c110585caf60f8bff0d72bd150d
y6m92 <y6m92@students.keele.ac.uk>
2026-03-09 00:19:13 +0000

    Add maximum attempts limit

--- game.py
+++ game.py
@@ -4,20 +4,23 @@ print("Welcome to the Number Guessing Game!")
 
 number = random.randint(1,20)
 attempts = 0 
-guess = 0
+max_attempts = 5
+guess_input = 0
 
 print("I am thinking of a number between 1 to 20.")
+print("You have 5 attempts to guess the number.")
 
-while guess_input != number:
-    guess = input("Enter your guess:")
+while guess_input != number and attempts < max_attempts: 
+    guess = input("Enter your guess: ")
     
     if not guess.isdigit():
-        print("Please enter a number.")
+        print("Please enter a valid number.")
         continue
     
     guess_input = int(guess)
     attempts += 1
     
+    #Game Logic
     if guess_input < number:
         print("Too low! Try again..")
     elif guess_input > number:
@@ -25,5 +28,10 @@ while guess_input != number:
     else:
         print("Congratulations! You guessed it right.")
         print("Number of attempts: ", attempts)
+        break
+    
+    if attempts == max_attempts:
+        print("You have used all your attempts. Game over!")
+        print("The correct number was:",number)
 
 
